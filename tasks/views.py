d9247c7854c35ee98f731ddc30b094bc0662d88f
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-12-26 17:31:23 +0600

    django template set up

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return HttpResponse ('welcome to the world')
 
-def show_task(request):
-    return HttpResponse("show all tasks")
+def manager_dashboard(request):
+    return render(request,"dashboard.html")
+
+def user_dashboard(request):
+    return render(request,'user-dashboard.html')
