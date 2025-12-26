d0f24af02e53546ae782f94d13c5b593e0803792
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-12-26 19:11:51 +0600

    django template set up

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def manager_dashboard(request):
-    return render(request,"dashboard.html")
+    return render(request,"dashboard/manager-dashboard.html")
 
 def user_dashboard(request):
-    return render(request,'user-dashboard.html')
+    return render(request,'dashboard/user-dashboard.html')
