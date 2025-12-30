ab3eb7732ea7aeb4ad30e9e8120b3a16829a590e
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-12-30 15:47:00 +0600

    add relationships,model,fields,reversing

--- tasks/models.py
+++ tasks/models.py
@@ -17,6 +17,7 @@ class Task(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
+
 class TaskDetail(models.Model):
     PRIORITY_OPTIONS=(
         ('H','High'),
