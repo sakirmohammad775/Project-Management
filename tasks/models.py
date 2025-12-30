8c30050a0324aa4e4c2d7ebb4d987ddc5cb6d3c8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-12-30 13:56:04 +0600

    add relationships,model,fields,reversing

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    
 
 class Task(models.Model):
+    project=models.ForeignKey("Project",on_delete=models.CASCADE,default=1) # many to one
+    
+    assigned_to=models.ManyToManyField(Employee,related_name='tasks')
     title=models.CharField(max_length=250)
     description=models.TextField()
     due_date=models.DateField()
@@ -18,3 +25,26 @@ class TaskDetail(models.Model):
     )
     assigned_to=models.CharField(max_length=100)
     priority=models.CharField(max_length=1,choices=PRIORITY_OPTIONS,default="L")
+
+class TaskDetail(models.Model):
+    HIGH='H' 
+    MEDIUM='M'
+    LOW='L'
+    
+    PRIORITY_OPTIONS=(
+        (HIGH,"High"),
+        (MEDIUM,"Medium"),
+        (LOW,"Low")
+        
+    )
+    task=models.OneToOneField(Task,on_delete=models.CASCADE,related_name='details') #
+    assigned_to=models.CharField(max_length=100)
+    priority=models.CharField(max_length=1,choices=PRIORITY_OPTIONS,default='L')
+    
+#Task.objects.get(id=2)
+#select*from task where id=2
+#ORM=
+
+class Project(models.Model):
+    name=models.CharField(max_length=100)
+    start_date=models.DateField()
