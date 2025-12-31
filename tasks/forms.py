9029b8d00783021feaa98110dd9dcf87828f72a2
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-12-31 11:57:33 +0600

    add task form

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,4 +3,6 @@ from django import forms
 class TaskForm (forms.Form):
     title=forms.CharField(max_length=250 ,label='Task Tile')
     description=forms.CharField(widget=forms.Textarea,label='Task Description')
-    
+    due_date=forms.DateField(widget=forms.SelectDateWidget,label="Due Date")
+    assigned_to=forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
+
