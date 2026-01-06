707e4c408db6ee112595a4a83a61f380a8f31ba1
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-01-07 01:30:46 +0600

    add some commit for remember

--- tasks/models.py
+++ tasks/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100) #store employee name
+    email = models.EmailField(unique=True) # store  unique email(no duplicate employees)
 
     def __str__(self):
-        return self.name  #
+        return self.name  #defines how object appears in admin shell
 
 
-class Task(models.Model):
-    STATUS_CHOICES = [
+class Task(models.Model): #represents an individual task under a project
+    STATUS_CHOICES = [      #choice restrict values and help  Ui Consistency
         ("PENDING", "pending"),
         ("IN_PROGRESS", "In Progress"),
         ("COMPLETED", "completed"),
     ]
-    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1)
-    assigned_to = models.ManyToManyField(Employee, related_name="tasks")
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1) #Many to One  relationship ,many tasks belong to one project, delete tasks if project is deleted
+    assigned_to = models.ManyToManyField(Employee, related_name="tasks") # M2M relationships ,
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
@@ -47,6 +47,6 @@ class TaskDetail(models.Model):
 
 # Represents a project under which multiple tasks can exist.
 class Project(models.Model):
-    name = models.CharField(max_length=100)
-    description=models.TextField(blank=True,null=True)
+    name = models.CharField(max_length=100) #project name
+    description=models.TextField(blank=True,null=True) # Optional project description # blank=True → form validation # null=True → database allows NULL
     start_date = models.DateField()
