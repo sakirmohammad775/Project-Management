9f4be6d74ddedcdfae5174321561ea015b51edd4
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-01-09 10:27:23 +0600

    add crud operation

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from tasks.forms import TaskModelForm, TaskForm, TaskDetailModelForm
 from tasks.models import Employee, Task, TaskDetail, Project
@@ -53,11 +53,49 @@ def create_task(request):
             task_detail = task_detail_form.save(commit=False)
             task_detail.task = task
             task_detail.save()
-            messages.success(request,"Task Added Successfully")
-            return redirect('create-task')
+            messages.success(request, "Task Added Successfully")
+            return redirect("create-task")
 
     context = {"task_form": task_form, "task_detail_form": task_detail_form}
     return render(request, "task_form.html", context)
+
+
+def update_task(request, id):
+    # employees=Employee.objects.all()
+    task = Task.objects.get(id=id)
+    task_form = TaskModelForm(instance=task)  # For GET
+
+    if task.details:
+        task_detail_form = TaskDetailModelForm(instance=task.details)
+
+    if request.method == "POST":  # For POST
+        task_form = TaskModelForm(request.POST, instance=task)
+        task_detail_form = TaskDetailModelForm(request.POST, instance=task.details)
+
+        if task_form.is_valid() and task_detail_form.is_valid():
+
+            """For Model form data"""
+            task_form.save()
+            task_detail = task_detail_form.save(commit=False)
+            task_detail.task = task
+            task_detail.save()
+
+            messages.success(request, "Task Updated Successfully")
+            return redirect("update-task", id)
+
+    context = {"task_form": task_form, "task_detail_form": task_detail_form}
+    return render(request, "task_form.html", context)
+
+
+def delete_task(request, id):
+    if request.method == "POST":
+        task = Task.objects.get(id=id)
+        task.delete()
+        messages.success(request, "Task Deleted Successfully")
+        return redirect("manager-dashboard")
+    else:
+        messages.error(request,'something went wrong')
+        return redirect('manager-dashboard')
 
 
 def view_task(request):
