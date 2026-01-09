a24f8d911e12cfd9aa0ef8a47642336ae3ef1bd6
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-01-09 11:18:51 +0600

    add crud operation

--- task_management/settings.py
+++ task_management/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-5ks#tdr!vu$myj*#vzs(_5h7qulvvvoe@3f6xc8fgm2unz*@go
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#For deployment
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
