7fc6dabf1b96acf11f55cd4fcd97b1c0ed5c422a
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-05 00:10:47 +0600

    add details views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from tasks.views import dashboard,manager_dashboard,employee_dashboard,create_task,view_task,update_task,delete_task,task_details,CreateTask,UpdateTask,ViewProject
+from tasks.views import dashboard,manager_dashboard,employee_dashboard,create_task,view_task,update_task,delete_task,task_details,CreateTask,UpdateTask,ViewProject,TaskDetail
 urlpatterns = [
     path('manager-dashboard/',manager_dashboard,name="manager-dashboard"),
     path('user-dashboard/',employee_dashboard,name='user-dashboard'),
@@ -8,7 +8,8 @@ urlpatterns = [
     path("create-task/", CreateTask.as_view(), name="create-task"),
     # path('view-task/',view_task,name='view-task'),
     path('view-task/',ViewProject.as_view(),name='view-task'),
-    path('task/<int:task_id>/details/',task_details,name='task-details'),
+    # path('task/<int:task_id>/details/',task_details,name='task-details'),
+    path('task/<int:task_id>/details/',TaskDetail.as_view(),name='task-details'),
     # path('update-task/<int:id>/',update_task,name='update-task'),
     path('update-task/<int:id>',UpdateTask.as_view(),name='update-task'),
     path('delete-task/<int:id>/',delete_task,name='delete-task'),
--- tasks/views.py
+++ tasks/views.py
@@ -16,7 +16,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic.base import ContextMixin
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 def is_manager(user):
     return user.groups.filter(name="Manager").exists()
@@ -248,6 +248,24 @@ def task_details(request, task_id):
     )
 
 
+class TaskDetail(DetailView):
+    model=Task
+    template_name='task_details.html'
+    context_object_name='task'
+    pk_url_kwarg='task_id'
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['status_choices']=Task.STATUS_CHOICES
+        return context
+    
+    def post(self,request,*args,**kwargs):
+        task=self.get_object()
+        selected_status=request.POST.get('task_status')
+        task.status=selected_status
+        task.save()
+        return redirect('task-details',task.id)
+    
 @login_required
 def dashboard(request):
     if is_manager(request.user):
@@ -257,3 +275,4 @@ def dashboard(request):
     elif is_admin(request.user):
         return redirect("admin-dashboard")
     return redirect("no-permission")
+ 
